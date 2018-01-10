57650b3de93a24325e4cc284bb347009f12a63e1
sanskarsharma <sanskar2996@gmail.com>
2018-01-11 03:52:45 +0530

    before making base template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app_instance
+from flask import render_template
 
 @app_instance.route('/')
 @app_instance.route('/index')		# these are called decorators
 def index():		# this is called a view function
-	return "hello from flasky"
+	user = {"username": "sanskarssh"}
+	posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+	return render_template("index.html", title = "bambadbonga", user = user, postslist = posts)
