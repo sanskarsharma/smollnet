def586d45481da4cc58743cfd66dbcc37385ef8d
sanskarsharma <sanskar2996@gmail.com>
2018-01-11 21:51:31 +0530

    webforms done

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 app_instance = Flask(__name__)
-
+app_instance.config.from_object(Config)		# setting the Config class from config.py module in our flask app object (app_instance)
+											# NOW our flask app (or our app_instance) knows where to look for config variables (i.e in object of Config class)
 from app import routes 	# this app meams app naam ka package i.e our directory which is named app
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app_instance
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 @app_instance.route('/')
 @app_instance.route('/index')		# these are called decorators
@@ -16,3 +17,13 @@ def index():		# this is called a view function
         }
     ]
 	return render_template("index.html", title = "bambadbonga", user = user, postslist = posts)
+
+
+
+@app_instance.route('/login', methods = ["GET", "POST"])
+def login():
+	form_obj = LoginForm()
+	if form_obj.validate_on_submit():
+		flash("Login requested for user {} , remember me = {}".format(form_obj.username.data, form_obj.remember_me.data))
+		return redirect(url_for("index"))
+	return render_template("login.html", title= "Sign In", form = form_obj)
