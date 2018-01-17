79ded3545c5f19257b73996ae5a2f872b54c8aaf
sanskarsharma <sanskar2996@gmail.com>
2018-01-17 19:01:24 +0530

    user profile page added

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login_manager_instance
+from hashlib import md5
 
 class User(UserMixin, db_instance.Model):
     id = db_instance.Column(db_instance.Integer, primary_key = True)
@@ -19,6 +20,10 @@ class User(UserMixin, db_instance.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode("utf-8")).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 
 class Post(db_instance.Model):
--- app/routes.py
+++ app/routes.py
@@ -60,3 +60,14 @@ def register_user():
         flash("Congratulations, you have been registered")
         return redirect(url_for("login"))
     return render_template("register.html", title = "Register yourself", form = registrattion_form_obj)
+
+
+@app_instance.route("/user/<username>")
+@login_required
+def user_profile(username):
+    user_obj = User.query.filter_by(username= username).first_or_404()
+    posts = [
+        {'author': user_obj, 'body': 'Test post #1'},
+        {'author': user_obj, 'body': 'Test post #2'}
+    ]
+    return render_template("user_profile.html", user = user_obj, posts = posts)
