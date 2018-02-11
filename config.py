61b91bb955d6ede583b21543c09f10fca7aa0b6f
sanskarsharma <sanskar2996@gmail.com>
2018-02-12 00:02:27 +0530

    mysql server replaces sqlite, overall refresh

--- config.py
+++ config.py
@@ -4,8 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 	SECRET_KEY = os.environ.get("SECRET_KEY") or "fallback-secret-key-which-you-cannot-guess"
 
-	SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql.2996@localhost/flaskdb'
+	
+	#os.environ.get("DATABASE_URL") or \
+        #'sqlite:///' + os.path.join(basedir, 'app.db')
 	
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
