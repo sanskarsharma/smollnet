ff565f9cb94ddbfc788cfbff8ca6faf0644b0c66
sanskarsharma <sanskar2996@gmail.com>
2018-09-25 18:04:10 +0530

    email back to using Threading, try

--- app/email.py
+++ app/email.py
@@ -5,33 +5,31 @@ from flask import render_template
 from threading import Thread
 
 
-# def send_email(subject, sender, recipients, text_body, html_body):
+def send_email(app_instance_here, msg):
     
-#     send_async_email_task.delay(subject=subject,sender=sender,recipients=recipients,text_body=text_body,html_body=html_body)
-    
+    with app_instance_here.app_context():
+        mail_instance.send(msg)
 
     # starting a thread instead of directly calling mail_instance.send() function on our main application thread
     # notice the target of thread is our send_async_email() function which in turn calls mail_instance.send()
-    # Thread(target= send_async_email, args=(app_instance, msg)).start()
 
 
-@celery.task
+# @celery.task
 def send_async_email_task(subject, sender, recipients, text_body, html_body):
-    #print("hello")
+    # print("hello")
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
 
-    with app_instance.app_context():
-        mail_instance.send(msg)
+    Thread(target=send_email, args=(app_instance, msg)).start()
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_async_email_task.delay(subject = '[smollnet] Reset Your Password',
-               sender= app_instance.config['ADMINS'][0],
-               recipients= [user.email],
-               text_body= render_template('email/reset_password_mail.txt', user=user, token=token),
-               html_body= render_template('email/reset_password_mail.html', user=user, token=token)
-               )
+    send_async_email_task(subject = '[smollnet] Reset Your Password',
+       sender= app_instance.config['ADMINS'][0],
+       recipients= [user.email],
+       text_body= render_template('email/reset_password_mail.txt', user=user, token=token),
+       html_body= render_template('email/reset_password_mail.html', user=user, token=token)
+       )
 
